f27b1d56388277054efab2427b1bfbcb306385c6
jKutllovci <jon.kutllovci@gmail.com>
2024-07-02 21:55:45 +0200

    still deployment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styled, { keyframes, css } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const ProjectsSection = styled.section`
   padding: 40px 0;
